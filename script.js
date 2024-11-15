804037b86ecc43625529ddec856a416205190843
Nadeem Ahmad <127886628+nadeemahmad9@users.noreply.github.com>
2024-11-15 18:58:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let cityInput = document.getElementById("city_input"),
   searchBtn = document.getElementById("searchBtn"),
   locationBtn = document.getElementById("locationBtn"),
-  api_key = "bdbb40a00c62e2e328c3b5da19f01bde",
+  api_key = "Your Api Key",
   currentWeatherCard = document.querySelectorAll(".weather-left .card")[0],
   fiveDaysForecastCard = document.querySelector(".day-forecast"),
   aqiCard = document.querySelectorAll(".highlights .card")[0],
